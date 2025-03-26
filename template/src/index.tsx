6912d48ec07f39dfb6df8abc118a85c1ffc4fe2e
yogeshkumarasamy <yogeshkumarasamy@gmail.com>
2025-03-26 13:10:33 +0530

    added .vscode recommendations and settings

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,15 +1,15 @@
 // import 'core-js/stable';
 // import 'regenerator-runtime/runtime';
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
 // Note: Using an Alias in Webpack
-import App from 'components/App/';
+import App from "components/App/";
 
 // Note: Using an Alias in Webpack
-import 'styles/index.scss';
-const domNode = document.getElementById('root');
+import "styles/index.scss";
+const domNode = document.getElementById("root");
 if (domNode) {
   const root = createRoot(domNode);
 
